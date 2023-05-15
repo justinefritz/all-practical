bb281271171e1a5299ce48033bd23f28093c9629
justinefritz <130990708+justinefritz@users.noreply.github.com>
2023-05-15 20:59:37 +0800

    Add files via upload

--- allpractical.c
+++ allpractical.c
@@ -9,12 +9,10 @@ void circle(){
     scanf("%f", &r);
     system("cls");
     radius = (3.14 * (r * r));
-    printf("The radius is %.2f\n\n", radius);
-    main();
+    printf("The radius is %.2f", radius);
 }
-
 void factorial(){
-    int num, total = 1;
+    long num, total = 1;
     printf("Enter the number : ");
     scanf("%d", &num);
     system("cls");
@@ -27,10 +25,8 @@ void factorial(){
         printf("%d * ", num);
         }
     }
-    printf("= %d\n\n", total);
-    main();
+    printf("= %d", total);
 }
-
 void multi(){
     int num = 1, total;
     printf("Enter the number : ");
@@ -43,10 +39,7 @@ void multi(){
         }
     printf("\n");
     }
-    printf("\n");
-    main();
 }
-
 void summa(){
     int num = 0, total = 0, totalnum;
     printf("Enter the number : ");
@@ -58,12 +51,10 @@ void summa(){
             printf("%d + ", num);
         }
         else{
-            printf("%d = %d\n\n", num, total);
-            main();
+            printf("%d = %d", num, total);
         }
     }
 }
-
 void rectangle(){
     float l, w, area;
     printf("Enter the length : ");
@@ -72,10 +63,8 @@ void rectangle(){
     scanf("%f", &w);
     system("cls");
     area = (l * w);
-    printf("The area of rectangle is %.2f\n\n", area);
-    main();  
+    printf("The area of rectangle is %.2f", area); 
 }
-
 void asterisk(){
     int ast;
     printf("Enter the height of triangle : ");
@@ -88,9 +77,7 @@ void asterisk(){
         printf("\n");
     }
     printf("\n");
-    main();
 }
-
 void fibo(){
     int nth, x = -1, y = 1, next;
     printf("Enter the nth term : ");
@@ -101,12 +88,10 @@ void fibo(){
         x = y;
         y = next;
     }
-    printf("%d term of fibonacci sequence is %d\n\n", nth, next);
-    main();
+    printf("%d term of fibonacci sequence is %d", nth, next);
 }
-
 void pascal(){
-    int nth, x = 0, y = 1, next;
+    long nth, x = 0, y = 1, next;
     printf("Enter the nth term : ");
     scanf("%d", &nth);
     system("cls");
@@ -120,10 +105,8 @@ void pascal(){
             next = next * 2;
         }
     }
-    printf("%d term of pascal sequence is %d\n\n", nth, next);
-    main();
+    printf("%d term of pascal sequence is %d", nth, next);
 }
-
 void bingo(){
     char* type;
     char letter;
@@ -133,153 +116,112 @@ void bingo(){
     system("cls");
     if (numb >= 1 && numb <= 75){
     type = numb % 2 == 0 ? "even" : "odd";
-        if (numb <= 15){
-            letter = 'B';
-            }
-            else if (numb >= 16 && numb <= 30){
-                letter = 'I';
-                }
-                else if (numb >= 31 && numb <= 45){
-                    letter = 'N';;
-                    } 
-                    else if (numb >= 46 && numb <= 60){
-                        letter = 'G';
-                        } 
-                        else if (numb >= 61 && numb <= 75){
-                            letter = 'O';
-                            }
+        if (numb <= 15) letter = 'B';
+            else if (numb >= 16 && numb <= 30) letter = 'I';
+                else if (numb >= 31 && numb <= 45) letter = 'N';
+                    else if (numb >= 46 && numb <= 60) letter = 'G';
+                        else if (numb >= 61 && numb <= 75) letter = 'O';
+    system("cls");
+    printf("%d is a an %s number that belongs to letter %c\n", numb, type, letter);
+    bingo();
     }
-    else if (numb < 1){
+    else if (numb < 0){
         reverse == abs(numb);
         system("cls");
         printf("%d is not a valid input and its absolute value is %d", numb, reverse);
         bingo();
             }
-            else{
+            else if (numb == 0 || numb > 75){
                 system("cls");
                 printf("Goodbye");
                 }
-    system("cls");
-    printf("%d is a an %s number that belongs to letter %c", numb, type, letter);
-    main();
 }
-
 void evenodd(){
-    int numb, choice, reverse;
+    int numb, reverse, choice;
     printf("Enter the integer : ");
     scanf("%d", &numb);
     system("cls");
     if (numb >= 1){
-        printf("1. Get Even Number\n");
-        printf("2. Get Odd Number\n");
-        printf(" : ");
-        scanf("%d", choice);
+        printf("1. Get Even Number\n2. Get Odd Number\n : ");
+        scanf("%d", &choice);
         system("cls");
         if (choice == 1 ){
             for (int w =1 ; w <= numb ; w++){
                 if (w%2 == 0){
                     printf("%d\n", w);
-                }
+                    }
             }
-        system("cls");
-        evenodd();
+            evenodd();
         }
         else if (choice == 2){
             for (int w = 1; w <= numb ; w++){
                 if (w%2 != 0){
-                    printf("%d\n", w);
+                        printf("%d\n", w);
                 }
             }
-        system("cls");
-        evenodd();
+            evenodd();
         }
         else{
             system("cls");
             printf("Invalid Input!.....");
             evenodd();
-        }
+            }
     }
-    else if (numb < 1){
+    else if (numb < 0){
         reverse = abs(numb);
         system("cls");
         printf("%d\n", reverse);
         evenodd();
-    }
-    else if (numb == 0){
-        system("cls");
-        printf("Goodbye!");
-    }
+        }
+        else if (numb == 0){
+            system("cls");
+            printf("Goodbye!");
+            }
 }
-
 void dice(){
     int roll, rand_num, total = 0, choice;
     srand(time(NULL));
-    printf("How many times would you like to roll the dice?\n");
-    scanf("%d", &roll);
-    printf("Rolling the dice %d times\n", roll);
-    for (int w = 1; w <= roll; w++){
-        rand_num = (rand() % 6) + 1;
-        printf("Roll %d : %d\n", w, rand_num);
-        total = total + rand_num;
-    }
-    printf("Total score : %d", total);
-    while (choice != 1 && choice != 0){
-        printf("\nDo you want to try again? 1. Yes  0. No\n");
-        scanf("%d", &choice);
-        switch(choice){
-            case 1:
-                system("cls");
-                dice();
-            case 0:
-                break;
-            default:
-                system("cls");
-                printf("\nYou've entered the wrong key, try again....");
+    while (choice > 0){
+        printf("How many times would you like to roll the dice?\n");
+        scanf("%d", &roll);
+        printf("Rolling the dice %d times\n", roll);
+        for (int w = 1; w <= roll; w++){
+            rand_num = (rand() % 6) + 1;
+            printf("Roll %d : %d\n", w, rand_num);
+            total = total + rand_num;
         }
+    printf("Total score : %d\nDo you want to try again? 1. Yes  0. No\n", total);
+    scanf("%d", &choice);
+    system("cls");
     }
 }
-
 void nine(){
-    int choice, rad, total, reverse;
+    int choice = 1, rad, total, reverse;
     srand(time(NULL));
-    printf("Your hand is: ");
-    for (int x = 1; x <= 3; x++){
-        rad = (rand() % 10) + 1;
-        printf("%d ", rad);
-        total = total + rad;
-    }
-    if (total >= 10 && total <= 19){
-    total = total - 10;
-    }
-    else if (total >= 20 && total <= 29){
-        total = total - 20;
-    }
-    else if (total > 30){
-        total = total - 30;
-    }
-    printf("\nYour final score is: %d\n", total);
-    if (total == 9){
-        printf("Congratulations, you win!");
-    }
-    else {
-        printf("Sorry, you lose.");
-    }
-    while (choice != 1 && choice != 0){
-        printf("\nDo you want to try again? 1. Yes  0. No\n");
-        scanf("%d", &choice);
-        switch(choice){
-            case 1:
-                system("cls");
-                dice();
-            case 0:
-                break;
-            default:
-                system("cls");
-                printf("\nYou've entered the wrong key, try again....");
+    while (choice > 0){
+        printf("Your hand is: ");
+        for (int x = 1; x <= 3; x++){
+            rad = (rand() % 10) + 1;
+            printf("%d ", rad);
+            total += rad;
         }
+        if (total >= 10 && total <= 19){
+            total -= 10;
+            }
+            else if (total >= 20 && total <= 29){
+                total -= 20;
+                }
+                else if (total > 30){
+                    total -= 30;
+                    }
+    printf("\nYour final score is: %d\n", total);
+    if (total == 9) printf("Congratulations, you win!");
+        else printf("Sorry, you lose.");
+    printf("\nDo you want to try again? 1. Yes  0. No\n");
+    scanf("%d", &choice);
+    system("cls");
     }
 }
-
 void guess(){
     int rand_num, guess1, total  = 0;
     srand(time(NULL));
@@ -289,85 +231,58 @@ void guess(){
         rand_num = (rand() % 5) + 1;
         if (guess1 < rand_num){
             printf("Too low, Try again!\n");
-            total = total + 1;
+            total++;
             }
             else if (guess1 > rand_num){
                 printf("Too high, try again!\n");
-                total = total + 1;          
+                total++;          
                 }
     }
-    total = total + 1;
+    total += 1;
     system("cls");
     printf("Congratulations, you guessed the number in %d guesses!", total);
-    main();
 }
-
 void card(){
-    int guess, ran_num, total = 0, choice;
+    int guess, ran_num, total = 0, choice = 1;
     srand(time(NULL));
+    while (choice >= 1 && choice != 0){
     for (int w = 1; w <= 2; w++){
         ran_num = (rand() % 13 ) + 1;
         printf("Card %d : %d\n", w, ran_num);
-        total = total + ran_num;
+        total += ran_num;
     }
-    if (total > 21){
-        printf("You won!");
-        } 
-        else if ( total == 21){
-            printf("You win the bonus prize!");
-            } 
-            else {
-                printf("You lost!");
-                }
+    if (total > 21) printf("You won!"); 
+        else if ( total == 21) printf("You win the bonus prize!");
+            else printf("You lost!");
     printf("\nDo you still want to play?  1 = Yes  0 = No\n");
     scanf("%d", &choice);
-    switch(choice){
-        case 1: 
-            system("cls");
-            main();
-        case 0:
-            system("cls");
-            break;
-        default:
-            system("cls");
-            printf("\nYou've entered the wrong key, try again....");
-            card();
     }
-}
-
+    printf("Thank you for using the program...");
+}    
 void mineral(){
     int perfect = 0, acceptable = 0, rejected = 0, choice = 1, ran1, ran2;
         while (choice >= 1 && choice != 0){
             srand(time(NULL));
-            ran1 = (rand() % 1000);
-            ran2 = (rand() % 100);
-            printf("Minerals have arrived: %d\n", ran1);
-            printf("Purity: %d percent\n", ran2);
+            ran1 = (rand() % 1000), ran2 = (rand() % 100);
+            printf("Minerals have arrived: %d\nPurity: %d percent\n", ran1, ran2);
             if (ran2 > 90){
                 printf("Perfect.\n");
-                perfect = perfect + ran1;
+                perfect += ran1;
                 }
                 else if (ran2 >= 70 && ran2 <= 89){
                     printf("Acceptable.\n");
-                    acceptable = acceptable + ran1;
+                    acceptable +=  ran1;
                     }
                     else {
                         printf("Rejected.\n");
-                        rejected = rejected + ran1;
+                        rejected += ran1;
                         }
-        printf("Total minerals PERFECT: %d, ACCEPTALBE: %d, REJECTED: %d", perfect, acceptable, rejected);
-        printf("\n\nContinue? ");
+        printf("Total minerals PERFECT: %d, ACCEPTALBE: %d, REJECTED: %dn\nContinue? ", perfect, acceptable, rejected);
         scanf("%d", &choice);
         system("cls");
-        switch (choice){
-            case 0:
-                system("cls");
-                printf("Thank you for using the program...");
-                break;  
         }
-    }
+    printf("Thank you for using the program...");
 }
-
 void immigrants(){
     int rand_num, children = 0, adolescents = 0, adult = 0, choice = 1;
     while (choice >= 1 && choice != 0){
@@ -376,50 +291,19 @@ void immigrants(){
         for (int w = 1 ; w <= 5; w++){
             rand_num = (rand() % 100);
             printf("%d ", rand_num);
-            if (rand_num >= 1 && rand_num <= 12){
-                children = children + 1;
-                }
-                else if (rand_num >= 13 && rand_num <= 17 ){
-                    adolescents = adolescents + 1;
-                    }
-                    else{
-                        adult = adult + 1;
-                        }
+            if (rand_num >= 1 && rand_num <= 12) children++;
+                else if (rand_num >= 13 && rand_num <= 17 ) adolescents++;
+                    else adult++;
         }
-    printf("\nTotal ADULT are: %d, Total ADOLESCENT are: %d, Total CHILDREN are: %d", adult, adolescents, children);
-    printf("\nDo you still want to accept immigrants? ");
+    printf("\nTotal ADULT are: %d, Total ADOLESCENT are: %d, Total CHILDREN are: %d\nDo you still want to accept immigrants? ", adult, adolescents, children);
     scanf("%d", &choice);
-    switch(choice){
-        case 0:
-            system("cls");
-            printf("Thank you for using the program....");
-            break;
-        default:
-            system("cls");
-        }
     }
+    printf("Thank you for using the program...");
 }
-
-int main(){
+int main(void){
     int choice;
-    printf("1. Radius\n");
-    printf("2. Factorial\n");
-    printf("3. Multiplication Table\n");
-    printf("4. Summation of Number\n");
-    printf("5. Rectangle\n");
-    printf("6. Triangle(*)\n");
-    printf("7. Fibonnaci Sequence\n");
-    printf("8. Pascal Sequence\n");
-    printf("9. Bingo\n");
-    printf("10. Even or Odd\n");
-    printf("11. Dice\n");
-    printf("12. Lucky Nine\n");
-    printf("13. Guess\n");
-    printf("14. Card\n");
-    printf("15. Mineral\n");
-    printf("16. Immigrants\n");
-    printf("17. Exit\n");
-    printf(" : ");
+    printf("1. Radius\n2. Factorial\n3. Multiplication Table\n4. Summation of Number\n5. Rectangle\n6. Triangle(*)\n7. Fibonnaci Sequence\n");
+    printf("8. Pascal Sequence\n9. Bingo\n10. Even or Odd\n11. Dice\n12. Lucky Nine\n13. Guess\n14. Card\n15. Mineral\n16. Immigrants\n17. Exit\n : ");
     scanf("%d", &choice);
     if(choice == 1){
         system("cls");
